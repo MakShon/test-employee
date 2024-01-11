a185e461aee4bfb3a723f15fc0dd5f459f6ff42b
MakShon <baran.maksym@student.uzhnu.edu.ua>
2024-01-11 12:09:27 +0200

    Initial commit

---
+++ src/main/java/com/example/test/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.test.service;
+
+import com.example.test.entity.EmployeeEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeService {
+    List<EmployeeEntity> findAllEmployee();
+    Optional<EmployeeEntity> findById(Long id);
+    EmployeeEntity saveEmployee(EmployeeEntity employeeEntity);
+    EmployeeEntity updateEmployee(EmployeeEntity employeeEntity);
+    void deleteEmployee(long id);
+}
---
+++ src/main/java/com/example/test/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.test.service.impl;
+
+import com.example.test.entity.EmployeeEntity;
+import com.example.test.repository.EmployeeRepository;
+import com.example.test.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<EmployeeEntity> findAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Optional<EmployeeEntity> findById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    @Override
+    public EmployeeEntity saveEmployee(EmployeeEntity employeeEntity) {
+        return employeeRepository.save(employeeEntity);
+    }
+
+    @Override
+    public EmployeeEntity updateEmployee(EmployeeEntity employeeEntity) {
+        return employeeRepository.save(employeeEntity);
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+        employeeRepository.deleteById(id);
+    }
+}
